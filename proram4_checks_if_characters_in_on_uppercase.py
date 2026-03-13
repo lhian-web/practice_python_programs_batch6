ad1d462d45d6c1eb9eddccafdbcf9d1513d8f346
unknown <jhullianchica@gmail.com>
2026-03-13 09:51:21 +0800

    Added input, check if character is in upper or lowercase, prints result

--- proram4_checks_if_characters_in_on_uppercase.py
+++ proram4_checks_if_characters_in_on_uppercase.py
@@ -1,3 +1,12 @@
 # Input text
+text = input("Enter your text here: ")
+
+upper = True
+
 # Check characters
+for char in text:
+    if char >= 'a' and char <= 'z':
+        upper = False
+
 # Print result
+print(upper)

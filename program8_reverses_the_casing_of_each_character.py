1668568cbfa6686a22c5d13753be33afaa8c23ed
unknown <jhullianchica@gmail.com>
2026-03-13 10:44:26 +0800

    Added input, checked each character, convert, print result

--- program8_reverses_the_casing_of_each_character.py
+++ program8_reverses_the_casing_of_each_character.py
@@ -1,3 +1,17 @@
 # Input text
+text = input("Enter text: ")
+result = ""
+
 # Check each character
+for char in text:
+    if char >= 'A' and char <= 'Z':
+        result = result + chr(ord(char) + 32)
+
+    elif char >= 'a' and char <= 'z':
+        result = result + chr(ord(char) - 32)
+
+    else:
+        result = result + char
+
 # Print result
+print(result)

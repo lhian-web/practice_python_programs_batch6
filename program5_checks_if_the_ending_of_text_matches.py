f1a6242c4ec5c52a449426788eb17130ad0ac2fc
unknown <jhullianchica@gmail.com>
2026-03-13 10:01:35 +0800

    Added input, ending word, check if matches, print result

--- program5_checks_if_the_ending_of_text_matches.py
+++ program5_checks_if_the_ending_of_text_matches.py
@@ -1,4 +1,11 @@
 # Input text
+text = input("Input your text here: ")
+
 # Input ending word
+ending_word = input("Input ending word here: ")
+
 # Check if ending matches
-# Print result
+if text[-len(ending_word):] == ending_word:
+    print("Ending word matches") # Print result
+else:
+    print("Ending word not matches")

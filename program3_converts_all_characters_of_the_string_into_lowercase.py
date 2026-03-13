d96c5f37cea5516fd2e156db5ca21a4714b8b843
unknown <jhullianchica@gmail.com>
2026-03-13 09:45:35 +0800

    Added input, converted uppercase to lowercase, print result

--- program3_converts_all_characters_of_the_string_into_lowercase.py
+++ program3_converts_all_characters_of_the_string_into_lowercase.py
@@ -1,3 +1,14 @@
 # Input text
+text = input("Enter your text here: ")
+
+result = ""
+
 # Convert uppercase to lowercase
+for char in text:
+    if char >= "A" and char <= "Z":
+        result = result + chr(ord(char) + 32)
+    else:
+        result = result + char
+
 # print result
+print(result)

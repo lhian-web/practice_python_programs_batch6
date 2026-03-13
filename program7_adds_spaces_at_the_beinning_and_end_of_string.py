d7597390684f89d1a0b5383f8c312f7c4151ef07
unknown <jhullianchica@gmail.com>
2026-03-13 10:25:45 +0800

    Added input, length limit, spaces on both sides, print result

--- program7_adds_spaces_at_the_beinning_and_end_of_string.py
+++ program7_adds_spaces_at_the_beinning_and_end_of_string.py
@@ -1,4 +1,20 @@
 # Input text
+text = input("Enter text: ")
+
 # Length limit
+limit = int(input("Enter length limit: "))
+
+spaces = limit - len(text)
+
+left = spaces // 2
+right = spaces -left
+
 # Add spaces on both sides
+for i in range(left):
+    text = " " + text
+
+for i in range(right):
+    text = text + " "
+
 # Print
+print(text)

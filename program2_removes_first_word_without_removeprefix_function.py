6d39e584e5e0926fd826f373e503a018c41a9fb5
unknown <jhullianchica@gmail.com>
2026-03-13 09:24:12 +0800

    Added input,checked if it matches the first word, removed space, printed result

--- program2_removes_first_word_without_removeprefix_function.py
+++ program2_removes_first_word_without_removeprefix_function.py
@@ -1,4 +1,17 @@
 # Input text
+text = input("Please input your text here: ")
 # Which word to remove
+word_remove = input("Please input the word to remove here: ")
+
 # Check if it matches first word in the text
+if text[0:len(word_remove)] == word_remove:
+    result = text[len(word_remove):]
+
+    if result[0] == " ":
+        result = result[1:]
+
+else:
+    result = text
+
 # Print
+print(result)

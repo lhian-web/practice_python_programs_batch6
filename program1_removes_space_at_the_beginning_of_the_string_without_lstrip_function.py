0492668ac4cec4cad702f9f285cb017011e316bd
unknown <jhullianchica@gmail.com>
2026-03-13 09:03:25 +0800

    Added input, check each character, start printing

--- program1_removes_space_at_the_beginning_of_the_string_without_lstrip_function.py
+++ program1_removes_space_at_the_beginning_of_the_string_without_lstrip_function.py
@@ -1,3 +1,12 @@
 # input text
+text = input("Please input your text with spaces at th beginning: ")
+
 # check each character
-# print
+start = False
+
+for char in text:
+    if char != " ":
+        start = True
+
+    if start: # print
+        print(char, end="")

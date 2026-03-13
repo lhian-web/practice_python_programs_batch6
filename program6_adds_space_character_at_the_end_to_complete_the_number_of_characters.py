a778d8436333e52b809f3cb7a633f6cc1dcd9f0e
unknown <jhullianchica@gmail.com>
2026-03-13 10:09:50 +0800

    Added input, limit, added spaces, print result

--- program6_adds_space_character_at_the_end_to_complete_the_number_of_characters.py
+++ program6_adds_space_character_at_the_end_to_complete_the_number_of_characters.py
@@ -1,4 +1,12 @@
 # Input text
+text = input("Enter a text: ")
+
 # Specify length
+limit = int(input("Enter length limit: "))
+
 # Add spaces until specified length is reached
+while len(text) < limit:
+    text = text + " "
+
 # Print result
+print(text)

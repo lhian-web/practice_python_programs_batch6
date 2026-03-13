a72480c3cfce73997515a178d1891b59bdbb5d09
unknown <jhullianchica@gmail.com>
2026-03-13 10:52:12 +0800

    Added input, checked each character, converted letters, print result

--- program9_capitalizes_first_character_of_strings.py
+++ program9_capitalizes_first_character_of_strings.py
@@ -1,4 +1,20 @@
 # Input text
+text = input("Enter text: ")
+result = ""
+
 # Check each character
-# Convert letters
+for i in range(len(text)):
+    char = text[i]
+
+    # Convert letters
+    if i == 0 and char >= "a" and char <= "z":
+        result = result + chr(ord(char) - 32)
+
+    elif i > 0 and char >= "A" and char <= "Z":
+        result = result + chr(ord(char) + 32)
+
+    else:
+        result = result + char
+
 # print result
+print(result)
